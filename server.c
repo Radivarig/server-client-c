7443ea951279c59bf57de0c3fafcf47a708bb83b
Radivarig <reslav.hollos@gmail.com>
2014-12-22 04:19:34 +0100

    [add]some comments [chg]improved error()

--- server.c
+++ server.c
@@ -5,31 +5,41 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <time.h>
 
-void error(const char *msg)
+void error(char *_msg)
 {
+    int len = strlen(_msg) + 9; 
+    char msg[len];
+    strcpy(msg, "Error: ");
+    strcat(msg, _msg);
+    strcat(msg, "\n");
     perror(msg);
     exit(1);
 }
 
+//argc is number or arguments, argv is array of arrays (of arguments)
 int main(int argc, char *argv[])
 {
-    int sock_fd, new_sock_fd;  //file descriptors
+    //time_t start_time = time(NULL);
+    //double allowed_time = 200;
+
+    int sock_fd, new_sock_fd;  //file descriptor, index of entry in kernel-resident array data structure that contains details of open files
     int port_no;
     socklen_t cli_len;
     char buffer[256];
     struct sockaddr_in serv_addr, cli_addr;
-    if (argc < 2) {
-       fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
+    
+    if (argc < 2)
+        error("no port provided");
 
+    //SOCK_STREAM is UTP, SOCK_DGRAM is DCP
     sock_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (sock_fd < 0) 
-       error("ERROR opening socket");
+       error("opening socket");
     
     bzero((char *) &serv_addr, sizeof(serv_addr));
-    port_no = atoi(argv[1]);   //string to int
+    port_no = atoi(argv[1]);   //arrayofchars to int
     
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
@@ -40,9 +50,14 @@ int main(int argc, char *argv[])
             (struct sockaddr *) &serv_addr,
             sizeof(serv_addr))
         < 0)
-        error("ERROR on binding");
+        error("on binding");
     
 
+    /*double passed_sec = 0;
+    while(passed_sec < 5){
+        passed_sec = difftime(time(NULL), start_time);
+    }*/
+    
     listen(sock_fd, 5);
     cli_len = sizeof(cli_addr);
     
@@ -52,20 +67,19 @@ int main(int argc, char *argv[])
         &cli_len
     );
 
-    if (new_sock_fd < 0) 
-        error("ERROR on accept");
+    if (new_sock_fd < 0) error("on accept");
     
     bzero(buffer,256);
     
     int n = read(new_sock_fd, buffer, 255);
-    if (n < 0) error("ERROR reading from socket");
+    if (n < 0) error("reading from socket");
     
     printf("Here is the message: %s\n", buffer);
-    
-    n = write(new_sock_fd,"I got your message", 18);
-    if (n < 0) error("ERROR writing to socket");
+   
+    n = write(new_sock_fd,"bla", 18);
+    if (n < 0) error("writing to socket");
     
     close(new_sock_fd);
     close(sock_fd);
-    return 0; 
+    return 0;
 }
