0a158ce389c763ba11079875cb95677bd04167d6
Radivarig <reslav.hollos@gmail.com>
2014-12-29 22:15:32 +0100

    [chg]tabs to spaces

--- v2/listener.c
+++ v2/listener.c
@@ -16,94 +16,94 @@
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa){
-	if (sa->sa_family == AF_INET)
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+    if (sa->sa_family == AF_INET)
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
 int main(void)
 {
-	int sockfd;								//socket file descritor, integer
-	struct addrinfo hints,					//data of type addrinfo 
-		   *servinfo; 						//pointer to data of type addrinfo that will be filled 
-	struct sockaddr_storage their_addr;		
-	char buf[MAXBUFLEN];					//buffer
-	socklen_t addr_len;						//unsigned opaque integer length of at least 32 bits
-	char s[INET6_ADDRSTRLEN];				//INET6_ADDRSTRLEN constant int for IPv6 addrstrlen, > IPv4 addrstrlen
+    int sockfd;                             //socket file descritor, integer
+    struct addrinfo hints,                  //data of type addrinfo 
+           *servinfo;                       //pointer to data of type addrinfo that will be filled 
+    struct sockaddr_storage their_addr;     
+    char buf[MAXBUFLEN];                    //buffer
+    socklen_t addr_len;                     //unsigned opaque integer length of at least 32 bits
+    char s[INET6_ADDRSTRLEN];               //INET6_ADDRSTRLEN constant int for IPv6 addrstrlen, > IPv4 addrstrlen
 
-	memset(&hints, 0, sizeof hints);		//set all bytes to zero
-	hints.ai_family = AF_UNSPEC;			//integer, any AF_INET or AF_INET6, address family
-	hints.ai_socktype = SOCK_DGRAM;			//integer, socket datagram
-	hints.ai_flags = AI_PASSIVE; 			//integer, address info, use my IP
+    memset(&hints, 0, sizeof hints);        //set all bytes to zero
+    hints.ai_family = AF_UNSPEC;            //integer, any AF_INET or AF_INET6, address family
+    hints.ai_socktype = SOCK_DGRAM;         //integer, socket datagram
+    hints.ai_flags = AI_PASSIVE;            //integer, address info, use my IP
 
-	int rv;
-	if (
-		(rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo))		!= 0) 
-	{
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+    int rv;
+    if (
+        (rv = getaddrinfo(NULL, MYPORT, &hints, &servinfo))     != 0) 
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-	// loop through all the results and bind to the first we can
-	struct addrinfo *p;
-	for(p = servinfo;    p != NULL;    p = p->ai_next)
-	{
-		if (
-			(sockfd = socket(p->ai_family, 
-							 p->ai_socktype,
-							 p->ai_protocol)) 	== -1)
-		{
-			perror("listener: socket");
-			continue;
-		}
+    // loop through all the results and bind to the first we can
+    struct addrinfo *p;
+    for(p = servinfo;    p != NULL;    p = p->ai_next)
+    {
+        if (
+            (sockfd = socket(p->ai_family, 
+                             p->ai_socktype,
+                             p->ai_protocol))   == -1)
+        {
+            perror("listener: socket");
+            continue;
+        }
 
-		if (
-			bind(sockfd,
-				 p->ai_addr,
-				 p->ai_addrlen) == -1)
-		{
-			close(sockfd);
-			perror("listener: bind");
-			continue;
-		}
+        if (
+            bind(sockfd,
+                 p->ai_addr,
+                 p->ai_addrlen) == -1)
+        {
+            close(sockfd);
+            perror("listener: bind");
+            continue;
+        }
 
-		break;
-	}
+        break;
+    }
 
-	if (p == NULL) {
-		fprintf(stderr, "listener: failed to bind socket\n");
-		return 2;
-	}
+    if (p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket\n");
+        return 2;
+    }
 
-	freeaddrinfo(servinfo);
+    freeaddrinfo(servinfo);
 
-	printf("listener: waiting to recvfrom...\n");
+    printf("listener: waiting to recvfrom...\n");
 
-	addr_len = sizeof their_addr;
+    addr_len = sizeof their_addr;
 
-	int numbytes;
-	if (
-		(numbytes = recvfrom(sockfd,
-							 buf,
-							 MAXBUFLEN -1,
-							 0,
-							 (struct sockaddr *)&their_addr,
-							 &addr_len)) 							== -1)
-	{
-		perror("recvfrom");
-		exit(1);
-	}
+    int numbytes;
+    if (
+        (numbytes = recvfrom(sockfd,
+                             buf,
+                             MAXBUFLEN -1,
+                             0,
+                             (struct sockaddr *)&their_addr,
+                             &addr_len))                            == -1)
+    {
+        perror("recvfrom");
+        exit(1);
+    }
 
-	printf("listener: got packet from %s\n", inet_ntop(their_addr.ss_family,
-													   get_in_addr((struct sockaddr *)&their_addr),
-													   s,
-													   sizeof s) );
-	
-	printf("listener: packet is %d bytes long\n", numbytes);
-	buf[numbytes] = '\0';
-	printf("listener: packet contains \"%s\"\n", buf);
+    printf("listener: got packet from %s\n", inet_ntop(their_addr.ss_family,
+                                                       get_in_addr((struct sockaddr *)&their_addr),
+                                                       s,
+                                                       sizeof s) );
+    
+    printf("listener: packet is %d bytes long\n", numbytes);
+    buf[numbytes] = '\0';
+    printf("listener: packet contains \"%s\"\n", buf);
 
-	close(sockfd);
+    close(sockfd);
 
-	return 0;
+    return 0;
 }
--- v2/talker.c
+++ v2/talker.c
@@ -15,62 +15,62 @@
 
 int main(int argc, char *argv[])
 {
-	int sockfd;
-	struct addrinfo hints, 
-		   *servinfo;
+    int sockfd;
+    struct addrinfo hints, 
+           *servinfo;
 
-	if (argc != 3) {
-		fprintf(stderr,"usage: talker hostname message\n");
-		exit(1);
-	}
+    if (argc != 3) {
+        fprintf(stderr,"usage: talker hostname message\n");
+        exit(1);
+    }
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_DGRAM;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
 
-	int rv;
-	if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+    int rv;
+    if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-	// loop through all the results and make a socket
-	struct addrinfo *p;
-	for(p = servinfo;    p != NULL;    p = p->ai_next)
-	{
-		if (
-			(sockfd = socket(p->ai_family,
-							 p->ai_socktype,
-							 p->ai_protocol))    == -1)
-		{
-			perror("talker: socket");
-			continue;
-		}
+    // loop through all the results and make a socket
+    struct addrinfo *p;
+    for(p = servinfo;    p != NULL;    p = p->ai_next)
+    {
+        if (
+            (sockfd = socket(p->ai_family,
+                             p->ai_socktype,
+                             p->ai_protocol))    == -1)
+        {
+            perror("talker: socket");
+            continue;
+        }
 
-		break;
-	}
+        break;
+    }
 
-	if (p == NULL) {
-		fprintf(stderr, "talker: failed to bind socket\n");
-		return 2;
-	}
+    if (p == NULL) {
+        fprintf(stderr, "talker: failed to bind socket\n");
+        return 2;
+    }
 
-	int numbytes;
-	if (
-		(numbytes = sendto(sockfd, argv[2],
-						   strlen(argv[2]),
-						   0,
-						   p->ai_addr,
-						   p->ai_addrlen))    == -1)
-	{
-		perror("talker: sendto");
-		exit(1);
-	}
+    int numbytes;
+    if (
+        (numbytes = sendto(sockfd, argv[2],
+                           strlen(argv[2]),
+                           0,
+                           p->ai_addr,
+                           p->ai_addrlen))    == -1)
+    {
+        perror("talker: sendto");
+        exit(1);
+    }
 
-	freeaddrinfo(servinfo);
+    freeaddrinfo(servinfo);
 
-	printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
-	close(sockfd);
+    printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
+    close(sockfd);
 
-	return 0;
+    return 0;
 }
